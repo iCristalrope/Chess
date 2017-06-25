ee2b9c2c4d6da6d59c247f4aae21579a902edcc1
Olivier De Cannière <olivi@MSI.lan>
2017-06-25 22:54:48 +0200

    board pieceable

--- Chess/src/Model/Board.java
+++ Chess/src/Model/Board.java
@@ -6,13 +6,34 @@ package Model;
  * @author Olivier De Cannière
  */
 public class Board {
-    public static final int maxRows = 8;
-    public static final int maxColumns = 8;
+
+    public static final int MAX_ROWS = 8;
+    public static final int MAX_COLUMNS = 8;
 
     Pieceable[][] pieces;
 
     public Board() {
-        pieces = new Pieceable[maxRows][maxColumns];
+        pieces = new Pieceable[MAX_ROWS][MAX_COLUMNS];
+    }
+
+    /**
+     * Moves a piece on the board according to the rules and takes the piece at the destination if necessary
+     *
+     * @param board the board containing the pieces
+     * @param origin the tile on which the moving piece starts
+     * @param destination the tile on which the moving piece ends
+     */
+    public void move(Board board, Coordinates origin, Coordinates destination) {
+
+    }
+
+    /**
+     * Returns a piece at a position on the board
+     *
+     * @param coord the position of the piece to return
+     * @return the piece at coord
+     */
+    public Pieceable getPiece(Coordinates coord) {
+        return pieces[coord.getRow()][coord.getColumn()];
     }
 }
-
--- Chess/src/Model/Pieceable.java
+++ Chess/src/Model/Pieceable.java
@@ -4,26 +4,33 @@ import java.util.ArrayList;
 
 /**
  * The interface all the pieces of the game implement. It standardises the methods among the pieces
- * 
+ *
  * @author Olivier De Cannière
  */
 public interface Pieceable {
-    
-   /**
-    * Returns the list of tiles reachable by simple displacement of a piece of this type at that position
-    * 
-    * @param board the board containing the pieces
-    * @param coord the tile on which the piece sits
-    * @return the list of coordinates reachable by the piece
-    */
-   ArrayList<Coordinates> captureable(Board board, Coordinates coord);
-       
-   /**
-    * Returns the list of tiles accessible by an attack of a piece of this type sitting at that position
-    * 
-    * @param board the board containing the pieces
-    * @param coord the tile on which the piece sits
-    * @return the list of coordinates attackable by the piece
-    */
-   ArrayList<Coordinates> accessible(Board board, Coordinates coord);
+
+    /**
+     * Returns the list of tiles reachable by simple displacement of a piece of this type at that position
+     *
+     * @param board the board containing the pieces
+     * @param coord the tile on which the piece sits
+     * @return the list of coordinates reachable by the piece
+     */
+    public ArrayList<Coordinates> captureable(Board board, Coordinates coord);
+
+    /**
+     * Returns the list of tiles accessible by an attack of a piece of this type sitting at that position
+     *
+     * @param board the board containing the pieces
+     * @param coord the tile on which the piece sits
+     * @return the list of coordinates attackable by the piece
+     */
+    public ArrayList<Coordinates> accessible(Board board, Coordinates coord);
+
+    /**
+     * Returns the color of this piece
+     *
+     * @return color
+     */
+    public Color getColor();
 }
