c1cca1616de05f82c004831eef13dd8cb68da870
olivi <olivi@MSI.lan>
2017-06-25 14:51:25 +0200

    debut projet organisation fichier test git

---
+++ Chess/src/Controller/Chess.java
@@ -0,0 +1,11 @@
+package Controller;
+
+/**
+ *
+ * @author Olivier De Cannière
+ */
+public class Chess {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
