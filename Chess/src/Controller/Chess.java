70835e2a09125d36770165cab9f6545a08959168
Olivier De Cannière <olivi@MSI.lan>
2017-06-28 21:33:27 +0200

    isKingInCheck affichage

--- Chess/src/Controller/Chess.java
+++ Chess/src/Controller/Chess.java
@@ -13,7 +13,10 @@ public class Chess {
 
     public static void main(String[] args) {
         Board board = new Board();
-        board.move(new Coordinates(1, 1), new Coordinates(2, 1));
+        board.move(new Coordinates(1, 4), new Coordinates(2, 4));
+        board.move(new Coordinates(0, 5), new Coordinates(3, 2));
+        board.move(new Coordinates(0, 3), new Coordinates(4, 7));
+        board.move(new Coordinates(4, 7), new Coordinates(6, 5));
 
         /* Piece piece = new King(Color.WHITE);
         board.putPiece(piece, new Coordinates(0, 1));
@@ -24,5 +27,7 @@ public class Chess {
         System.out.println(board);*/
         ConsoleDisplayBoard.init();
         ConsoleDisplayBoard.draw(board);
+        System.out.println("King at (0, 4) is in check : " + board.isKingInCheck(new Coordinates(0, 4)));
+        System.out.println("King at (7, 4) is in check : " + board.isKingInCheck(new Coordinates(7, 4)));
     }
 }
